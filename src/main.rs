56860115e612f9e15bddcd87a75298337fe1655d
Thomas Hurst <tom@hur.st>
2020-04-06 18:45:23 +0000

    Experimental slog conversion

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,12 @@
 use std::net::SocketAddr;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::{Duration, Instant};
+use std::process::exit;
 
-use env_logger;
 use exitcode;
 use futures::stream::StreamExt;
 use futures_util::future::FutureExt;
-use log::LevelFilter;
-use log::{error, info, warn};
+use slog::{info, warn, crit, o, Drain, Logger, Level};
 use structopt;
 use structopt::StructOpt;
 use tokio::io::AsyncWriteExt;
@@ -68,15 +67,9 @@ struct Config {
     #[structopt(long = "threads")]
     #[allow(clippy::option_option)]
     threads: Option<Option<usize>>,
-    /// Disable timestamps in logs
+    /// JSON logging
     #[structopt(long)]
-    disable_log_timestamps: bool,
-    /// Disable module name in logs (e.g. "tarssh")
-    #[structopt(long)]
-    disable_log_ident: bool,
-    /// Disable log level in logs (e.g. "info")
-    #[structopt(long)]
-    disable_log_level: bool,
+    log_json: bool,
     #[cfg(all(unix, feature = "drop_privs"))]
     #[structopt(flatten)]
     #[cfg(all(unix, feature = "drop_privs"))]
@@ -97,23 +90,27 @@ struct PrivDropConfig {
     chroot: Option<PathBuf>,
 }
 
-fn errx<M: AsRef<str>>(code: i32, message: M) -> ! {
-    error!("{}", message.as_ref());
-    std::process::exit(code);
+
+struct FDuration(Duration);
+
+impl std::fmt::Display for FDuration {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:.2?}", self.0)
+    }
 }
 
 async fn tarpit_connection(
     mut sock: tokio::net::TcpStream,
-    peer: SocketAddr,
+    log: Logger,
     delay: Duration,
     time_out: Duration,
 ) {
     let start = Instant::now();
     sock.set_recv_buffer_size(1)
-        .unwrap_or_else(|err| warn!("set_recv_buffer_size(), error: {}", err));
+        .unwrap_or_else(|err| warn!(log, "set_recv_buffer_size()"; "error" => %err));
 
     sock.set_send_buffer_size(16)
-        .unwrap_or_else(|err| warn!("set_send_buffer_size(), error: {}", err));
+        .unwrap_or_else(|err| warn!(log, "set_send_buffer_size()"; "error" => %err));
 
     for chunk in BANNER.iter().cycle() {
         delay_for(delay).await;
@@ -125,11 +122,8 @@ async fn tarpit_connection(
         if let Err(err) = res {
             let connected = NUM_CLIENTS.fetch_sub(1, Ordering::Relaxed) - 1;
             info!(
-                "disconnect, peer: {}, duration: {:.2?}, error: \"{}\", clients: {}",
-                peer,
-                start.elapsed(),
-                err,
-                connected
+                log, "disconnect";
+                "duration" => %FDuration(start.elapsed()), "error" => %err, "clients" => connected
             );
             break;
         }
@@ -143,51 +137,45 @@ fn main() {
     let delay = Duration::from_secs(opt.delay);
     let timeout = Duration::from_secs(opt.timeout);
     let log_level = match opt.verbose {
-        0 => LevelFilter::Off,
-        1 => LevelFilter::Info,
-        2 => LevelFilter::Debug,
-        _ => LevelFilter::Trace,
+        0 => Level::Critical,
+        1 => Level::Info,
+        2 => Level::Debug,
+        _ => Level::Trace,
     };
 
-    env_logger::Builder::from_default_env()
-        .filter(None, log_level)
-        .format_timestamp(if opt.disable_log_timestamps {
-            None
-        } else {
-            Some(env_logger::fmt::TimestampPrecision::Millis)
-        })
-        .format_module_path(!opt.disable_log_ident)
-        .format_level(!opt.disable_log_level)
-        .init();
+    let drain = if opt.log_json {
+        let drain = slog_json::Json::default(std::io::stdout()).fuse();
+        slog_async::Async::new(drain).build().filter_level(log_level).fuse()
+    } else {
+        let decorator = slog_term::TermDecorator::new().stdout().build();
+        let drain = slog_term::FullFormat::new(decorator).build().fuse();
+        slog_async::Async::new(drain).build().filter_level(log_level).fuse()
+    };
+
+    let log = slog::Logger::root(drain, o!());
 
     let mut rt = tokio::runtime::Builder::new();
-    let mut scheduler;
 
     if let Some(threaded) = opt.threads {
         rt.threaded_scheduler();
 
-        scheduler = "threaded".to_string();
-
         if let Some(threads) = threaded {
             let threads = threads.min(512).max(1);
             rt.core_threads(threads);
-            scheduler = format!("threaded, threads: {}", threads);
+            info!(log, "init"; "version" => env!("CARGO_PKG_VERSION"), "scheduler" => "threaded", "threads" => threads);
         }
     } else {
-        scheduler = "basic".to_string();
+        info!(log, "init"; "version" => env!("CARGO_PKG_VERSION"), "scheduler" => "basic");
         rt.basic_scheduler();
     }
-
-    info!(
-        "init, version: {}, scheduler: {}",
-        env!("CARGO_PKG_VERSION"),
-        scheduler
-    );
 
     let mut rt = rt
         .enable_all()
         .build()
-        .unwrap_or_else(|err| errx(exitcode::UNAVAILABLE, format!("tokio, error: {:?}", err)));
+        .unwrap_or_else(|err| {
+            crit!(log, "tokio"; "error" => %err);
+            exit(exitcode::UNAVAILABLE);
+        });
 
     let startup = Instant::now();
 
@@ -197,14 +185,12 @@ fn main() {
         .map(
             |addr| match rt.block_on(async { TcpListener::bind(addr).await }) {
                 Ok(listener) => {
-                    info!("listen, addr: {}", addr);
+                    info!(log, "listen"; "addr" => addr);
                     listener
                 }
                 Err(err) => {
-                    errx(
-                        exitcode::OSERR,
-                        format!("listen, addr: {}, error: {}", addr, err),
-                    );
+                    crit!(log, "listen"; "addr" => addr, "error" => %err);
+                    exit(exitcode::OSERR);
                 }
             },
         )
@@ -218,56 +204,59 @@ fn main() {
         {
             let mut pd = PrivDrop::default();
             if let Some(path) = opt.privdrop.chroot {
-                info!("privdrop, chroot: {}", path.display());
+                info!(log, "privdrop"; "chroot" => %path.display());
                 pd = pd.chroot(path);
             }
 
             if let Some(user) = opt.privdrop.user {
-                info!("privdrop, user: {}", user.to_string_lossy());
+                info!(log, "privdrop"; "user" => %user.to_string_lossy());
                 pd = pd.user(user);
             }
 
             if let Some(group) = opt.privdrop.group {
-                info!("privdrop, group: {}", group.to_string_lossy());
+                info!(log, "privdrop"; "group" => %group.to_string_lossy());
                 pd = pd.group(group);
             }
 
             pd.apply()
-                .unwrap_or_else(|err| errx(exitcode::OSERR, format!("privdrop, error: {}", err)));
+                .unwrap_or_else(|err| {
+                    crit!(log, "privdrop"; "error" => %err);
+                    exit(exitcode::OSERR);
+                });
 
-            info!("privdrop, enabled: true");
+            info!(log, "privdrop"; "enabled" => true);
         } else {
-            info!("privdrop, enabled: false");
+            info!(log, "privdrop"; "enabled" => false);
         }
     }
 
     #[cfg(all(unix, feature = "sandbox"))]
     {
         let sandboxed = Sandbox::new().sandbox_this_process().is_ok();
-        info!("sandbox, enabled: {}", sandboxed);
+        info!(log, "sandbox"; "enabled" => sandboxed);
     }
 
     info!(
-        "start, servers: {}, max_clients: {}, delay: {}s, timeout: {}s",
-        listeners.len(),
-        opt.max_clients,
-        delay.as_secs(),
-        timeout.as_secs()
+        log, "start";
+        "servers" => listeners.len(), "max_clients" => opt.max_clients,
+        "delay" => ?delay, "timeout" => ?timeout
     );
 
     for mut listener in listeners {
+        let log = log.clone();
         let server = async move {
             loop {
                 match listener.accept().await {
                     Ok((sock, peer)) => {
                         let connected = NUM_CLIENTS.fetch_add(1, Ordering::Relaxed) + 1;
+                        let clog = log.new(o!("peer" => peer));
 
                         if connected > max_clients {
                             NUM_CLIENTS.fetch_sub(1, Ordering::Relaxed);
-                            info!("reject, peer: {}, clients: {}", peer, connected);
+                            info!(clog, "reject"; "clients" => connected);
                         } else {
-                            info!("connect, peer: {}, clients: {}", peer, connected);
-                            tokio::spawn(tarpit_connection(sock, peer, delay, timeout));
+                            info!(clog, "connect"; "clients" => connected);
+                            tokio::spawn(tarpit_connection(sock, clog, delay, timeout));
                         }
                     }
                     Err(err) => match err.kind() {
@@ -276,7 +265,7 @@ fn main() {
                         | std::io::ErrorKind::ConnectionReset => (),
                         _ => {
                             let wait = Duration::from_millis(100);
-                            warn!("accept, err: {}, wait: {:?}", err, wait);
+                            warn!(log, "accept"; "error" => %err, "wait" => %FDuration(wait));
                             delay_for(wait).await;
                         }
                     },
@@ -292,23 +281,20 @@ fn main() {
 
         #[cfg(unix)]
         let mut term = signal(SignalKind::terminate()).unwrap_or_else(|error| {
-            errx(exitcode::UNAVAILABLE, format!("signal(), error: {}", error))
+            crit!(log, "signal()"; "error" => %error);
+            exit(exitcode::UNAVAILABLE);
         });
         #[cfg(unix)]
-        let term2 = term.recv().into_stream().map(|_| "terminated");
+        let term2 = term.recv().into_stream().map(|_| "term");
         #[cfg(unix)]
         let interrupt = futures_util::stream::select(interrupt, term2);
 
         if let Some(signal) = interrupt.boxed().next().await {
-            info!("{}", signal);
+            info!(log, "signal"; "kind" => signal);
         };
     };
 
     rt.block_on(shutdown);
 
-    info!(
-        "shutdown, uptime: {:.2?}, clients: {}",
-        startup.elapsed(),
-        NUM_CLIENTS.load(Ordering::Relaxed)
-    )
+    info!(log, "shutdown"; "uptime" => %FDuration(startup.elapsed()), "clients" => NUM_CLIENTS.load(Ordering::Relaxed));
 }
